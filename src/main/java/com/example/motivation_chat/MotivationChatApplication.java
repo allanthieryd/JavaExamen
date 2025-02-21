77492b5fec4312d454067f88e4560f362569b63f
allanthieryd <allan.td@free.fr>
2025-02-21 10:15:24 +0100

    First commit

---
+++ src/main/java/com/example/motivation_chat/MotivationChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.motivation_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivationChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivationChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/motivation_chat/MotivationChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motivation_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivationChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
